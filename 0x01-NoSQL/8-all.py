285ffe0234e73882a26fc316fdee4ffeae744400
Ernest-Geek <ernestampene1@gmail.com>
2024-03-26 14:55:15 +0000

    update

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""A python function that list all
+document in a collection
+"""
+import pymongo
+
+
+def list_all(mongo_collection):
+    """
+    function to list all doc in a collection
+    """
+    if not mongo_collection:
+        return []
+    documents = mongo.collection.find()
+    return [post for posts in document]
