9a338e9a9fa1e61bf2c3860ef9c1f3e4fea273e0
Ernest-Geek <ernestampene1@gmail.com>
2024-03-27 00:26:15 +0000

    update

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Returns the list of school having a specific topic
+"""
+import pymongo
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    find by specific value
+    """
+    return mongo_collection.find({"topics": {"$in": [topic]})
+
